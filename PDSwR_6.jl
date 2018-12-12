22c689da58b1b35ebfcfde0b32418527c08f493d
Morten Ørris Poulsen <morten.oerris.poulsen@LEGO.com>
2018-12-12 12:04:29 +0100

    Section 6.3 done

--- PDSwR_6.jl
+++ PDSwR_6.jl
@@ -88,8 +88,12 @@ train = @where(train_all, :rgroup .<= 0.8)
 
 # Print frequency table of Var218 ------------------------------------------
 
-# Fix to include missings
-FreqTables.freqtable(train, :Var218, :churn)
+# Calculate frequency table for Var218 split by churn
+table218 = FreqTables.freqtable(train, :Var218, :churn)
+
+# --------------------------------------------------------------------------
+# Title: Churn rates grouped by variable 218 codes
+table218[:, 2] ./ (table218[:, 1] .+ table218[:, 2])
 
 
 # --------------------------------------------------------------------------
@@ -100,15 +104,6 @@ FreqTables.freqtable(train, :Var218, :churn)
 # --------------------------------------------------------------------------
 # Original R Code ----------------------------------------------------------
 # --------------------------------------------------------------------------
-
-
-# --------------------------------------------------------------------------
-# (example 6.3 of section 6.2.1)  : Memorization methods : Building single-variable models : Using categorical features
-# Title: Churn rates grouped by variable 218 codes
-
-> print(table218[,2]/(table218[,1]+table218[,2]))
-      cJvF       UYBR       <NA>
-0.05994389 0.08223821 0.26523297
 
 
 # --------------------------------------------------------------------------
